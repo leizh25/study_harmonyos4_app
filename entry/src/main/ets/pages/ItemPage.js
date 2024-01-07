9182d6e182105d11abe60d391bfce841649eeb76
leizh25 <leizhuzheng@qq.com>
2024-01-07 15:43:55 +0800

    ArkUI组件-循环控制

---
+++ entry/src/main/ets/pages/ItemPage.js
@@ -0,0 +1,18 @@
+"use strict";
+class Item {
+    constructor(name, image, price) {
+        this.name = name;
+        this.image = image;
+        this.price = price;
+    }
+}
+struct ItemPage extends   {
+    constructor() { }
+    build() {
+            .width('100%')
+            .height('100%')
+            .backgroundColor('#efefef')
+            .padding(20);
+    }
+}
+//# sourceMappingURL=ItemPage.js.map

6022043c149da658dbfecc76ce43f118bfdc71b3
leizh25 <leizhuzheng@qq.com>
2024-01-08 16:34:16 +0800

    ArkUI-状态管理-页面路由

--- entry/src/main/ets/components/CommonComponent.js
+++
@@ -1,9 +0,0 @@
-export struct Header extends   {
-    constructor() { }
-    build() {
-        // 标题部分
-            .width('100%')
-            .height(30);
-    }
-}
-//# sourceMappingURL=CommonComponent.js.map
